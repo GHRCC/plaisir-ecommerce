601ce8499f1d3077445dc4d51e6cee7cf9149e9d
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-08-25 18:10:09 -0300

    List Products / Implement React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-      <h1>Plaisir</h1>
-      </Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import { Navbar, Nav, Container, NavbarBrand } from 'react-bootstrap'
-import {FaShoppingCart, FaUser} from 'react-icons/fa'
-import logo from "../assets/logo.jpg"
+import React from "react";
+import { Navbar, Nav, Container, NavbarBrand } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo.jpg";
 
 const Header = () => {
   return (
     <header>
-        <Navbar id='navbar' expand="lg" collapseOnSelect>
-            <Container id='container'>
-                <Navbar.Brand id='navbar' href="/">
-                    <img src={logo} alt="logo" width="100" height="100" className="d-inline-block align-top" />
-                    <h1>Plaisir</h1>
-                    </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link href="/cart"><FaShoppingCart/> Carrinho</Nav.Link>
-                    <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-
-            </Container>
-        </Navbar>
+      <Navbar id="navbar" expand="lg" collapseOnSelect>
+        <Container id="container">
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                src={logo}
+                alt="logo"
+                width="100"
+                height="100"
+                className="d-inline-block align-top"
+              />
+              <h1>Plaisir</h1>
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Carrinho
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/signin">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
