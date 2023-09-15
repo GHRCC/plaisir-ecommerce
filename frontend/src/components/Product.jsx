6a84235b52e521f187c63cea9cb7524d1fe56444
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-09-15 10:00:59 -0300

    Update

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,6 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import Rating from './Rating';
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
@@ -14,6 +14,9 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as='div'><Rating value={product.rating} text={`${product.numReviews} reviews`}>
+          </Rating>
+          </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
